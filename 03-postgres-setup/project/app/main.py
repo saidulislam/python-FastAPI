aecb367a072cfc19b6dd06b3b3ef8d0d6c51f5fc
saidulislam <saidul.islam@gmail.com>
2022-09-21 22:23:24 -0400

    Add files via upload

---
+++ 03-postgres-setup/project/app/main.py
@@ -0,0 +1,28 @@
+# project/app/main.py
+
+import os
+
+from fastapi import FastAPI, Depends
+from tortoise.contrib.fastapi import register_tortoise
+
+from app.config import get_settings, Settings
+
+app = FastAPI()
+
+
+register_tortoise(
+    app,
+    db_url=os.environ.get("DATABASE_URL"),
+    modules={"models": ["app.models.tortoise"]},
+    generate_schemas=False,  # updated
+    add_exception_handlers=True,
+)
+
+
+@app.get("/ping")
+async def pong(settings: Settings = Depends(get_settings)):
+    return {
+        "ping": "pong!",
+        "environment": settings.environment,
+        "testing": settings.testing
+    }
